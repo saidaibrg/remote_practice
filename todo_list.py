17f6d908efe5beddff186e4370bdce6dd82d923b
bluesibra <41315308+bluesibra@users.noreply.github.com>
2020-09-29 15:14:19 -0400

    added endline

--- todo_list.py
+++ todo_list.py
@@ -7,4 +7,4 @@ task=raw_input("Enter a task: ")
 task_list.append(task)
 print("Your tasks for today are:")
 #Printing the task list without brackets usuing .join(list) function 
-print(",".join(task_list))
+print(" -\n ".join(task_list))
