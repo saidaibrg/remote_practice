1d8558103a5d3e4c87e9ab3d949e9ad25ea0ac9a
bluesibra <41315308+bluesibra@users.noreply.github.com>
2020-09-29 14:35:10 -0400

    added the input

--- todo_list.py
+++ todo_list.py
@@ -2,3 +2,8 @@ from datetime import date
 
 today=date.today()
 print("Today is " + str(today)+".")
+task_list=[]
+task=raw_input("Enter a task: ")
+task_list.append(task)
+print("Your tasks for today are:")
+print(task_list)
